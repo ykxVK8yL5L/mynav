264d18658b6c8536f79b666bb945423e5a7cf91a
ykxVK8yL5L <58539637+ykxVK8yL5L@users.noreply.github.com>
2021-03-22 11:35:48 +0800

    Update menu.php

--- src/manage/view/menu.php
+++ src/manage/view/menu.php
@@ -14,7 +14,8 @@
             <!-- 右侧的导航项目 -->
             <ul class="nav navbar-nav navbar-right">
                 <li><a target="_blank" href="?control=html&action=flush&preview=true">预览效果</a></li>
-                <li><a href="?control=html&action=flush">重新生成</a></li>
+                <li><a href="?control=html&action=flush&tpl=template">生成模板1</a></li>
+                <li><a href="?control=html&action=flush&tpl=template2">生成模板2</a></li>
             </ul>
         </div><!-- END .navbar-collapse -->
     </div>
