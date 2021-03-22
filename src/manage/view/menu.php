5619e639bbef96301fdf85dc996f348798b164bc
ykxVK8yL5L <58539637+ykxVK8yL5L@users.noreply.github.com>
2021-03-22 20:20:49 +0800

    Update menu.php
    去除无用的预览并优化手机显示

--- src/manage/view/menu.php
+++ src/manage/view/menu.php
@@ -4,19 +4,20 @@
         <div class="navbar-header">
             <a class="navbar-brand" href="?control=index">导航管理</a>
         </div>
-        <!-- 导航项目 -->
-        <div class="collapse navbar-collapse navbar-collapse-example">
-            <ul class="nav navbar-nav navbar-left">
-                <li><a href="?control=nav&action=index">链接管理</a></li>
-                <li><a href="?control=category&action=index">分类管理</a></li>
 
-            </ul>
-            <!-- 右侧的导航项目 -->
-            <ul class="nav navbar-nav navbar-right">
-                <li><a target="_blank" href="?control=html&action=flush&preview=true">预览效果</a></li>
-                <li><a href="?control=html&action=flush&tpl=template">生成模板1</a></li>
-                <li><a href="?control=html&action=flush&tpl=template2">生成模板2</a></li>
-            </ul>
-        </div><!-- END .navbar-collapse -->
+
+    <ul class="nav nav-primary">
+      <li><a href="?control=nav&action=index">链接管理</a></li>
+      <li><a href="?control=category&action=index">分类管理</a></li>
+      <!--<li><a target="_blank" href="?control=html&action=flush&preview=true">预览效果</a></li>-->
+      <li>
+        <a class="dropdown-toggle" data-toggle="dropdown" href="#">生成 <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+           <li><a href="?control=html&action=flush&tpl=template">模板1</a></li>
+           <li><a href="?control=html&action=flush&tpl=template2">模板2</a></li>
+        </ul>
+      </li>
+    </ul>
+
     </div>
 </nav>
